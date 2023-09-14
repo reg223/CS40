562b87ed56c5a54244fd5c2ccf1151eacfe6c2a8
reg223 <reg22355@gmail.com>
2023-09-14 17:27:59 -0400

    CS40 work progress

---
+++ HW01/filesofpix/readaline.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "readaline.h"
+
+size_t readaline(FILE *inputfd, char **datapp){
+    printf("hello world!\n");
+    return 0;
+}
+
+
+
---
+++ HW01/filesofpix/restoration.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "readaline.h"
+
+
+int main(int argc, char *argv[]){
+    
+    (void)argc;
+    (void) argv;
+    printf("%lu",readaline());
+}
