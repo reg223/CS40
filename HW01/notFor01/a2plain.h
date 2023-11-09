521ec73f593f58b9001cbbc542a811e048876df9
reg223 <reg22355@gmail.com>
2023-11-09 12:38:00 -0500

    another clean up

--- HW01/notFor01/a2plain.h
+++
@@ -1,3 +0,0 @@
-#include <a2methods.h>
-
-extern A2Methods_T uarray2_methods_plain; /* functions for normal arrays */
--- HW01/notFor01/stackoverflow.h
+++
@@ -1,2 +0,0 @@
-extern void report_stack_overflow(void);
-  /* install handler: on stack overflow, write to stderr, and call abort() */
